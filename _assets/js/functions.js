6ce9f34579aee08f36a1cee4dc48fcdedda4185d
Dennis Fink <dennisfinkdn@web.de>
2018-01-26 23:41:16 +0100

    Initial commit
    Basic setup for the boilerplate

---
+++ _assets/js/functions.js
@@ -0,0 +1,6 @@
+console.log("Hello from functions.js!");
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+console.log("Fensterbreite: " + windowWidth + "px" + "\n" + "Fensterhöhe: " + windowHeight + "px");
